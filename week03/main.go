91e799f8ae494d72047aabe805bc7822d6213753
omu-ajh24046 <sg24575y@st.omu.ac.jp>
2025-11-25 01:11:51 +0000

    Pending changes exported from your codespace

--- week03/main.go
+++ week03/main.go
@@ -1,13 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
 
 func main() {
 	// Week 03: ここに課題のコードを記述してください
 	// 詳細な課題内容はLMSで確認してください
-	
+
 	fmt.Println("Week 03 課題")
-	
+
 	// 以下に実装してください
-	
+	http.HandleFunc("/webfortune", webfortunehandler)
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func webfortunehandler(w http.ResponseWriter, r *http.Request) {
+	seed := time.Now().UnixNano()
+	d := rand.New(rand.NewSource(seed))
+	randnum := d.Int31n(4)
+	switch randnum {
+	case 0:
+		fmt.Fprintln(w, "今日の運勢は大吉です")
+	case 1:
+		fmt.Fprintln(w, "今日の運勢は中吉です")
+	case 2:
+		fmt.Fprintln(w, "今日の運勢は吉です")
+	case 3:
+		fmt.Fprintln(w, "今日の運勢は凶です")
+	}
 }
